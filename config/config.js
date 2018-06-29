7456346f56af8e2f92f564a77b3ec23b05a343dd
fadhlimajid <fadhlimajid43@gmail.com>
2018-06-29 15:25:15 +0700

    for heroku

--- config/config.js
+++ config/config.js
@@ -6,6 +6,22 @@ module.exports = {
     "password": process.env.DB_PASS,
     "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST || "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "postgres",
+    "ssl": true,
+    "dialectOptions": {
+      "ssl": true
+    }
+  },
+  "production": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST || "127.0.0.1",
+    "port": 5432,
+    "dialect": "postgres",
+    "ssl": true,
+    "dialectOptions": {
+      "ssl": true
+    }
   }
 }
